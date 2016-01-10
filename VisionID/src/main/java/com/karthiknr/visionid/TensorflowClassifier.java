6f43b267e2165e594e2c227228ce0e7374db6745
Karthik Narumanchi <karthiknr@users.noreply.github.com>
2016-01-10 20:36:03 +0530

    Removed Action Bar Menu,Added Nav Header Background,Added Licenses,Added Version Info to Nav Drawer

--- VisionID/src/main/java/com/karthiknr/visionid/TensorflowClassifier.java
+++ VisionID/src/main/java/com/karthiknr/visionid/TensorflowClassifier.java
@@ -1,4 +1,5 @@
 /* Copyright 2015 Google Inc. All Rights Reserved.
+   Copyright 2015 Karthik Narumanchi
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
